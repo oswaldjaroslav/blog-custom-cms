5935320e10eae8105b49b4a31ee80376f02be90c
oswald <oswald.jaroslav@gmail.com>
2017-12-22 05:25:47 +0100

    Clickable admin dashboard

--- CMS/src/components/menu-bar.component.tsx
+++ CMS/src/components/menu-bar.component.tsx
@@ -2,18 +2,29 @@ import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationMenuIcon from 'material-ui/svg-icons/navigation/menu';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-export interface MenuBarProps {
-    handleMenuClick: Function;
+export interface MenuBarProps extends RouteComponentProps<any> {
+    handleMenuClick(): any
 }
 
-export const MenuBar = (props: MenuBarProps) =>
+const MenuBarBase = (props: MenuBarProps) =>
     <div className="menu-bar" >
         <AppBar 
-        title="Admin-Dashoboard" 
+        title={
+            <span 
+            style={{
+                cursor: 'pointer'
+            }}
+            onClick={() => { props.history.push('/admin') }} >
+                Admin-Dashboard
+            </span>
+        }
         iconElementLeft={
-            <IconButton onClick={() => { props.handleMenuClick() }} >
+            <IconButton onClick={ props.handleMenuClick } >
                 <NavigationMenuIcon/>
             </IconButton>
         } />
     </div>
+
+export const MenuBar = withRouter(MenuBarBase);
